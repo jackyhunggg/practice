9391783808769e8595e871fa7decb2ae75f53e5c
Jacky Hung <jacky85725@gmail.com>
2023-03-29 01:18:42 +0800

    set up server

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
 // only using dotenv while not in the production environment
-if(process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
-}
-
-// Define the parameters
-// Load express and build up the server
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const port = 3000
-// set connection to mongodb
-mongoose.connection = (process.env.MONGODB_URI)
-
-// set database connection status
-const db = mongoose.connection
-// connection error
-db.on('error', () => {
+  }
+  
+  // Define the parameters
+  // Load express and build up the server
+  const express = require('express')
+  const app = express()
+  const mongoose = require('mongoose')
+  const port = 3000
+  
+  // set up MongoDB connection
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  
+  // set database connection status
+  const db = mongoose.connection
+  
+  // connection error
+  db.on('error', () => {
     console.log('mongodb error!')
-})
-// connection success
-db.once('open', () => {
+  })
+  
+  // connection success
+  db.once('open', () => {
     console.log('mongodb connected!')
-})
-
-app.get('/', (req, res) => {
+  })
+  
+  app.get('/', (req, res) => {
     res.send('hello world')
-})
-
-app.listen(port, (req, res) => {
+  })
+  
+  app.listen(port, () => {
     console.log(`App is running on port ${port}`)
-})
+  })
+  
